16e29b8fd7c5da7b22aa80c3db87d7636f2d19f4
emanuelcula <47657407+emanuelcula@users.noreply.github.com>
2020-01-07 22:55:28 +0100

    Datenbank SQLITE beschreiben

--- random_warmup/testsqlite.py
+++ random_warmup/testsqlite.py
@@ -1,6 +1,29 @@
 import sqlite3
 
-connection = sqlite3.connect('random.db')
+#Datenbank erstellen
+connection = sqlite3.connect('exercises.db')
+cursor = connection.cursor()
 
-curser = connection.cursor()
+#Tabelle in Datenbank erstellen
+sql = '''CREATE TABLE IF NOT EXISTS Warmup 
+				(PID INTEGER PRIMARY KEY AUTOINCREMENT,
+				 BESCHREIBUNG VARCHAR(250),
+				 TEILNEHMER INT,
+				 MATERIAL VARCHAR(250))'''
 
+cursor.execute(sql)
+
+#Daten in Tabelle einfügen
+sql = "INSERT INTO Warmup (BESCHREIBUNG, TEILNEHMER, MATERIAL) VALUES ('TESTBESCHREIBUNG ERSTE UEBUNG', 6, 'KEINS')"
+
+cursor.execute(sql)
+connection.commit()
+sql = 'SELECT * FROM Warmup'
+cursor.execute(sql)
+
+rows = cursor.fetchall()
+
+for row in rows:
+	print(row)
+
+connection.close()
