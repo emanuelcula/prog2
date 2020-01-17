2a83b0b05f9a18d00505ef2b63323c966a7626de
emanuelcula <47657407+emanuelcula@users.noreply.github.com>
2020-01-17 11:49:19 +0100

    update main.py

--- warmup_exercises/flaskblog/__init__.py
+++ warmup_exercises/flaskblog/__init__.py
@@ -1,3 +1,4 @@
+#die __init__.py 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
--- warmup_exercises/main.py
+++ warmup_exercises/main.py
@@ -1,4 +1,7 @@
+#Die Funktionion sind in eigenen Packages gespeichert
+#über die main.py wird die Flask-App geladen und weitergeleitet
+
 from flaskblog import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
