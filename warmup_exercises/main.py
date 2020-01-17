a9c842519c4d4e92da0c932057bbe977610bef2f
emanuelcula <47657407+emanuelcula@users.noreply.github.com>
2020-01-17 09:38:50 +0100

    update blog

--- warmup_exercises/main.py
+++ warmup_exercises/main.py
@@ -1,33 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-posts = [
-	{
-		'author': 'Emanuel Cula',
-		'title': 'Übung 1',
-		'content': 'Erster Beitrag',
-		'date_posted': '1. Januar 2020'
-	},
-	{
-		'author': 'Cula Emanuel',
-		'title': 'Übung 2',
-		'content': 'Zweiter Beitrag',
-		'date_posted': '2. Januar 2020'
-	}
-]
-
-
-@app.route("/")
-@app.route("/home")
-def home():
-	return render_template('index.html', posts=posts)
-
-
-@app.route("/kontakt")
-def about():
-	return render_template ('kontakt.html', title='Kontakt')
-
+from flaskblog import app
 
 if __name__ == '__main__':
-	app.run()
+    app.run(debug=True)
